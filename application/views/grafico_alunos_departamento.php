3ab3aa4abb28a2302e4205771348a471b04c17b5
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 17:00:36 +0000

    graficos

--- application/views/grafico_alunos_departamento.php
+++ application/views/grafico_alunos_departamento.php
@@ -33,7 +33,7 @@ var myPieChart = new Chart(ctx, {
     labels: javascript_array,
     datasets: [{
       data: <?php echo json_encode(array_values($alunos_departamento));?>,
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745', '#19a745','#33b745', '#62a745'],
+      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745', '#003300','#999966','#ffcccc','#ff00ff','#6600cc'.'#669999'],
     }],
   },
 });
