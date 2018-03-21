f1c173ca93035845e20d5467f48980c5fd09e70e
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 21:56:47 +0000

    nav-bar

--- assets/js/sb-admin.js
+++ assets/js/sb-admin.js
@@ -8,8 +8,6 @@
   $("#sidenavToggler").click(function(e) {
     e.preventDefault();
    
-    $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
-    $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
   });
   // Force the toggled class to be removed when a collapsible nav link is clicked
   $(".navbar-sidenav .nav-link-collapse").click(function(e) {
