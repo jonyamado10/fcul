609afaac3d0c68b3862dfd31b2b5a4e6cc410b86
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 22:26:19 +0000

    nav-bar

--- application/views/nav.php
+++ application/views/nav.php
@@ -20,8 +20,8 @@
   <link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
 <script type="text/javascript">
   $( document ).ready(function() {
-    var isVisible = $( "#collapseComponents" ).is( ":visible" );
-    console.log( isVisible );
+    
+    console.log( $( "#collapseComponents" ).is( ":visible" ) );
 });
 </script>
 </head>
