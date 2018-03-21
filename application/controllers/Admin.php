38d053e40663c2c77466399f48b8ba28517d40ce
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 15:45:33 +0000

    graficos

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -51,6 +51,7 @@ class Admin extends CI_Controller {
 	
 	public function grafico_alunos_por_departamento()
 	{
+		$this->load->model('Users_model');
 
 		print_r($this->Users_model->get_num_alunos_por_departamento());
 	}
