8d0e00f5ea1143df7724a6b3cb369a23f2d76eaf
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 23:35:39 +0000

    nav-bar

--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -3,16 +3,11 @@ $(document).ready(function(){
     $("#navTabelas").click(function(){
      if ( $( "#collapseComponents2" ).is( ":visible" )) {
       
-      <?php 
-      $data = array('mostra_tabela' => 0 );
-      $this->session->set_userdata($data);
-      echo "alert(" . $this->session->userdata(); . ")";
-      ?>
+  			localStorage.setItem("navTabela", 0);
     }
     else{
 
-	
-      alert("escondeu");
+		localStorage.setItem("navTabela", 1);
 
     }
     });
