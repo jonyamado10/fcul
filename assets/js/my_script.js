4a3c4a77398cf95732ed7ecb1e681e3c0c009e6a
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 23:51:34 +0000

    nav-bar

--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -11,10 +11,11 @@ $(document).ready(function(){
 
     }
     });
-});
-    if(localStorage.getItem("navTabela") == 1 ){
+     if(localStorage.getItem("navTabela") == 1 ){
     	$( "#collapseComponents2" ).show();
     }
     else{
     	$( "#collapseComponents2" ).hide();
     }
+});
+
