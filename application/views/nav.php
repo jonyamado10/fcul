681b1e7976d15709a39d30482edf0e48fb348759
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-03-21 22:55:45 +0000

    nav-bar

--- application/views/nav.php
+++ application/views/nav.php
@@ -50,8 +50,8 @@
             </li>
           </ul>
         </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
-          <a id = "navTabelas" class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents2" data-parent="#exampleAccordion">
+        <li id = "navTabelas" class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+          <a  class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents2" data-parent="#exampleAccordion">
             <i class="fa fa-fw fa-table"></i>
             <span class="nav-link-text">Tabelas</span>
           </a>
