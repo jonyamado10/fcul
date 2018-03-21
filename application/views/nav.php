472cd9eae5a6c5d52655f731e6e68289841a5478
unknown <fn97@outlook.com>
2018-03-21 00:26:50 +0000

    a alterar a dashboard do admin 4.1

--- application/views/nav.php
+++ application/views/nav.php
@@ -24,7 +24,7 @@
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html"><img src = "<?php echo base_url('assets/img/logo.png') ?>" style = "width:auto; height:auto;"></a>
+    <a class="navbar-brand" href="index.html"><img src = "<?php echo base_url('assets/img/logo.png') ?>" style = "width:100%; height:5%;"></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
