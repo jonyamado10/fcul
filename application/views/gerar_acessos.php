3cdd870c0e245eae3d858888a9e589b5380108c3
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-06 21:16:54 +0100

    gerar acessos

--- application/views/gerar_acessos.php
+++ application/views/gerar_acessos.php
@@ -16,13 +16,18 @@
         echo form_open('Acessos/gerar_acessos','class="contact100-form validate-form"'); 
         echo validation_errors();
         ?>
-        <div class="form-group"> <?php echo form_input('data',$this->input->post('data'),'class="form-control", id="data", value="2018-08-19", type="date"');?>
+        <div class="form-group"> <?php echo form_input('data',$this->input->post('data'),'class="form-control", id="data", value="2018-08-19",type="date"');?>
           <span class="focus-input100"></span>
           <label class="col-2 col-form-label" for="data">
             <span class="glyphicon glyphicon-credit-card"></span>
           </label>
         </div>
-
+        <div class="form-group row">
+  <label for="example-date-input" class="col-2 col-form-label">Date</label>
+  <div class="col-10">
+    <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+  </div>
+</div>
 
 
         <div class="container-contact100-form-btn">
