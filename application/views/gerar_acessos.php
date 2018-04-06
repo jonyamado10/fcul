e9d69d5ef6bcf93db9fc8afbc659d5c5237010a7
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-06 21:18:23 +0100

    gerar acessos

--- application/views/gerar_acessos.php
+++ application/views/gerar_acessos.php
@@ -23,9 +23,9 @@
           </label>
         </div>
         <div class="form-group row">
-  <label for="example-date-input" class="col-2 col-form-label">Date</label>
+  <label for="aaaa" class="col-2 col-form-label">Date</label>
   <div class="col-10">
-    <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+    <input class="form-control" type="date" value="2011-08-19" id="aaa">
   </div>
 </div>
 
