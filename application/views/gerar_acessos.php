0794620956956ac46fbc8c956b301ef5d09129c0
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-06 22:23:46 +0100

    gerar acessos

--- application/views/gerar_acessos.php
+++ application/views/gerar_acessos.php
@@ -1,59 +1,5 @@
 
-<style type="text/css">
-.container-contact100-form-btn {
-  width: 100%;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  padding-top: 17px;
-}
 
-.contact100-form-btn {
-  font-family: Montserrat-Bold;
-  font-size: 12px;
-  color: #fff;
-  line-height: 1.2;
-  text-transform: uppercase;
-
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 20px;
-  min-width: 160px;
-  height: 42px;
-  border-radius: 21px;
-  background: #1560d8;
-
-  box-shadow: 0 10px 30px 0px rgba(132, 106, 221, 0.5);
-  -moz-box-shadow: 0 10px 30px 0px rgba(132, 106, 221, 0.5);
-  -webkit-box-shadow: 0 10px 30px 0px rgba(132, 106, 221, 0.5);
-  -o-box-shadow: 0 10px 30px 0px rgba(132, 106, 221, 0.5);
-  -ms-box-shadow: 0 10px 30px 0px rgba(132, 106, 221, 0.5);
-
-  -webkit-transition: all 0.4s;
-  -o-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  transition: all 0.4s;
-}
-
-.contact100-form-btn:hover {
-  background: #333333;
-  box-shadow: 0 10px 30px 0px rgba(51, 51, 51, 0.5);
-  -moz-box-shadow: 0 10px 30px 0px rgba(51, 51, 51, 0.5);
-  -webkit-box-shadow: 0 10px 30px 0px rgba(51, 51, 51, 0.5);
-  -o-box-shadow: 0 10px 30px 0px rgba(51, 51, 51, 0.5);
-  -ms-box-shadow: 0 10px 30px 0px rgba(51, 51, 51, 0.5);
-}
-
-</style>
 
 <div class="container-fluid">
       <!-- Breadcrumbs-->
