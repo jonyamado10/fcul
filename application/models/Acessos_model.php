4cf8d70036c22792e6c57c9b2fb6ce74d8ceb9a3
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-07 16:10:13 +0100

    gerar acessos

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -30,28 +30,7 @@ class Acessos_model extends CI_Model {
 		$query = $this->db->get(); 
         return $query->result_array();
     }
-    public function add_acesso(){
-		date_default_timezone_set('Europe/Lisbon');
-		$date = date('Y-m-d H:i:s');
-
-		$data = array(
-			'cardnumber' => $this -> input->post('cardnumber'),
-			'building' => $this -> input->post('edificio'),
-			'floornumber' => $this -> input->post('piso'),
-			'door' => $this -> input->post('porta'),
-			'date' => $date
-			);
-		$query = $this->db->insert('acessos',$data);
-		if($query){
-	
-
-			array_rand(input)
-			return true;
-		} else{
-			return false;
-		}
-
-    }
+   
     function gerar_acessos(){
     	$data = $this->input->post('data');
     	$acessos = array();
