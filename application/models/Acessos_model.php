a4fc43d5938730d5d51d6e479a08f0826b39e5aa
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-07 16:08:57 +0100

    gerar acessos

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -6,6 +6,30 @@ class Acessos_model extends CI_Model {
         $query = $this->db->get('acessos');
         return $query->result_array();
     }
+    function get_ids_acessos() {
+        $this->db->select('id');
+		$this->db->from('acessos');
+		$query = $this->db->get(); 
+        return $query->result_array();
+    }
+    function get_ids_alunos() {
+        $this->db->select('id');
+		$this->db->from('alunos');
+		$query = $this->db->get(); 
+        return $query->result_array();
+    }
+    function get_ids_docentes() {
+        $this->db->select('id');
+		$this->db->from('docentes');
+		$query = $this->db->get(); 
+        return $query->result_array();
+    }
+    function get_ids_nao_docentes() {
+        $this->db->select('id');
+		$this->db->from('nao_docentes');
+		$query = $this->db->get(); 
+        return $query->result_array();
+    }
     public function add_acesso(){
 		date_default_timezone_set('Europe/Lisbon');
 		$date = date('Y-m-d H:i:s');
@@ -19,6 +43,9 @@ class Acessos_model extends CI_Model {
 			);
 		$query = $this->db->insert('acessos',$data);
 		if($query){
+	
+
+			array_rand(input)
 			return true;
 		} else{
 			return false;
@@ -50,9 +77,53 @@ class Acessos_model extends CI_Model {
 		}
 		$query = $this->db->insert_batch('acessos', $acessos);
 		if($query){
-			return true;
+			$ids_alunos = $this->get_ids_alunos();
+			$ids_docentes = $this->get_ids_docentes();
+			$ids_nao_docentes = $this->get_ids_nao_docentes();
+			$acessos_alunos = array();
+			$acessos_docentes = array();
+			$acessos_nao_docentes = array();
+			shuffle($acessos);
+			$i=0;
+			foreach ($acessos as $acesso) {
+				if ($i< 3500) {
+					$id_aluno = array_rand($ids_alunos);
+					$acesso_aluno = array('id_acesso' => $acesso['id'] ,
+											'id_aluno' => $id_aluno );
+					array_push($acessos_alunos, $acesso_aluno);
+					
+				}
+				else if($i < 4500){
+					$id_docente = array_rand($ids_docentes);
+					$acesso_docente = array('id_acesso' => $acesso['id'] ,
+											'id_docente' => $id_docente );
+					array_push($acessos_docentes, $acesso_docente);
+
+				}
+				else{
+					$id_nao_docente = array_rand($ids_nao_docentes);
+					$acesso_nao_docente = array('id_acesso' => $acesso['id'] ,
+											'id_nao_docente' => $id_nao_docente );
+					array_push($acessos_nao_docentes, $acesso_nao_docente);
+				}
+				$i++;
+			}
+			$query1 = $this->db->insert_batch('acessos_alunos', $acessos_alunos);
+			$query2 = $this->db->insert_batch('acessos_docentes', $acessos_docentes);
+			$query3 = $this->db->insert_batch('acessos_nao_docentes', $acessos_nao_docentes);
+
+			if ($query1 and $query2 and $query3) {
+				return true;
+			}
+			else{
+				$this->db->trans_rollback();
+				return false;
+			}	
+		
+			
 		}
 		else{
+			$this->db->trans_rollback();
 			return false;
 		}
 
