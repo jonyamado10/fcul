aca3d351f3baa02d645f083907eb552334a9d02c
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-10 21:23:09 +0100

    gerar acessos

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -172,8 +172,7 @@ class Acessos_model extends CI_Model {
 						ORDER BY a.data DESC, a.hora DESC";
 			$query = $this->db->query($sql);
 			$result = $query->result_array();
-			print_r($result);
-			echo sizeof($result);
+	
     		print_r($this->corrige_acessos($result));
     		echo "<br><br>";
     			# code...
