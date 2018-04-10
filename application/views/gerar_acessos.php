bcdbdd3bef4c0dd32f178500beb4472f8f4dad13
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-10 17:26:24 +0100

    gerar acessos

--- application/views/gerar_acessos.php
+++ application/views/gerar_acessos.php
@@ -37,6 +37,19 @@
         <script>
   $('#BotaoGerar').click(function(){
       $('#lo').html("<div class='loader' style = 'width:120px; height:120px; margin:0 auto'> </div>Aguarde ");
+         var url = "<?php echo base_url('Acessos/acessos_validation') ?>"; // the script where you handle the form input.
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: $("data").serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               alert(data); // show response from the php script.
+           }
+         });
+
+    return false; // avoid to execute the actual submit of the form.
  
   });
         </script>
