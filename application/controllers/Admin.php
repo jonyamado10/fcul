b026cb05998f539565e9834bccc0f956679ee545
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-10 19:17:14 +0100

    gerar acessos

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,6 +21,8 @@ class Admin extends CI_Controller {
 	}
 	public function dashboard()
 	{
+		if (!$_SERVER['HTTP_REFERER']) $this->redirect('Admin/index');
+
 		$this->load->view('admin_dashboard');
 	}
 	public function tabela_alunos()
