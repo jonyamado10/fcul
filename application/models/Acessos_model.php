5e944d03d06e51fdc8d963e5a0782d244717dc5d
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-11 00:25:00 +0100

    gerar acessos

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -183,7 +183,7 @@ class Acessos_model extends CI_Model {
 			for ($i=0; $i <sizeof($acessos_por_pessoa)-1 ; $i++) { 
 			 	if($acessos_por_pessoa[$i]['sentido'] == "Entrada"){
 			 		if($acessos_por_pessoa[$i+1]['sentido'] == "Entrada"){ // temos que simular uma saida
-			 				$copia_acesso=$acessos_por_pessoa[$i+1];
+			 				$copia_acesso=new ArrayObject($acessos_por_pessoa[$i+1]);
 			 				$copia_acesso['sentido'] = "Saida";
 			 				$copia_acesso['id_acesso'] = -$copia_acesso['id_acesso'];
 			 				array_splice( $copia_acessos, $i+1, 0, $copia_acesso ); 
@@ -193,10 +193,10 @@ class Acessos_model extends CI_Model {
 			 	}
 			 	else{
 			 		if($acessos_por_pessoa[$i+1]['sentido'] == "Saida"){ // temos que simular uma entrada
-			 				$copia_acesso=$acessos_por_pessoa[$i+1];
+			 				$copia_acesso=ArrayObject($acessos_por_pessoa[$i+1]);
 			 				$copia_acesso['sentido'] = "Entrada";
 			 				$copia_acesso['id_acesso'] = -$copia_acesso['id_acesso'];
-			 				$copia_acesso2=$acessos_por_pessoa[$i];
+			 				$copia_acesso2=ArrayObject($acessos_por_pessoa[$i]);
 			 				$copia_acesso2['sentido'] = "Entrada";
 			 				$copia_acesso2['id_acesso'] = -$copia_acesso2['id_acesso'];
 			 				array_splice( $copia_acessos, $i+2, 0, $copia_acesso ); 
@@ -205,10 +205,10 @@ class Acessos_model extends CI_Model {
 			 		}
 			 		else{
 			 			if($acessos_por_pessoa[$i]['porta'] != $acessos_por_pessoa[$i+1]['porta'] ){
-			 				$copia_acesso= $acessos_por_pessoa[$i+1];
+			 				$copia_acesso= ArrayObject($acessos_por_pessoa[$i+1]);
 			 				$copia_acesso['sentido'] = "Saida";
 			 				$copia_acesso['id_acesso'] = -$copia_acesso['id_acesso'];
-			 				$copia_acesso2=$acessos_por_pessoa[$i];
+			 				$copia_acesso2=ArrayObject($acessos_por_pessoa[$i]);
 			 				$copia_acesso2['sentido'] = "Entrada";
 			 				$copia_acesso2['id_acesso'] = -$copia_acesso2['id_acesso'];
 			 			
@@ -222,7 +222,7 @@ class Acessos_model extends CI_Model {
 			}
 			else{
 				if($acessos_por_pessoa[0]['sentido'] == "Saida"){
-					$copia_acesso=$acessos_por_pessoa[0];
+					$copia_acesso=ArrayObject($acessos_por_pessoa[0]);
 			 		$copia_acesso['sentido'] = "Entrada";
 			 		$copia_acesso['id_acesso'] = -$copia_acesso['id_acesso'];
 			 		array_push($copia_acessos, $copia_acesso);
