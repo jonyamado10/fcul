8e95e0838e3466440f66c24c0caafc7b407b3323
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-10 21:22:16 +0100

    gerar acessos

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -182,7 +182,7 @@ class Acessos_model extends CI_Model {
 	function corrige_acessos($acessos_por_pessoa){
 		$copia_acessos = $acessos_por_pessoa;
 
-			for ($i=0; $i <sizeof($acessos_por_pessoa) ; $i++) { 
+			for ($i=0; $i <sizeof($acessos_por_pessoa)-1 ; $i++) { 
 			 	if($acessos_por_pessoa[$i]['sentido'] == "Entrada"){
 			 		if($acessos_por_pessoa[$i+1]['sentido'] == "Entrada"){ // temos que simular uma saida
 			 				$copia_acesso=$acessos_por_pessoa[$i+1];
