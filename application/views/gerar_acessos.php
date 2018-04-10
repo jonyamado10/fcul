e6a8eb85d27ca0924de4b62c531aa8d81e34029a
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-10 17:35:47 +0100

    gerar acessos

--- application/views/gerar_acessos.php
+++ application/views/gerar_acessos.php
@@ -27,8 +27,11 @@
         </div>
      
         <div class="container-contact100-form-btn">
-          <button id="BotaoGerar">cria</button>
-         
+          <?php  
+                  echo form_submit('gerar','Gerar', array('class' =>'contact100-form-btn', 'id' =>'BotaoGerar'));
+                  echo form_close(); 
+                           
+                    ?>
         </div>
         <div id = "lo"></div>
         <script>
