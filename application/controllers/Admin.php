8769f985a6b46ff6be69b0230d646a772c15b31c
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-11 01:55:43 +0100

    gerar acessos

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -101,27 +101,27 @@ class Admin extends CI_Controller {
 		foreach ($acessos as $acesso ) {
 			if ($acesso['id_acesso'] > 0) {
 				if ($acesso['sentido']=='Saida') {
-					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => red);
+					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => 'red');
 				}
 				else{
-					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => green);
+					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => 'green');
 				}
 				$this->table->add_row($acesso['num_aluno'],$acesso['data'],$acesso['hora'],$acesso['porta'],$cell, 'Sim');
 			}
 			else{
 				if ($acesso['sentido']=='Saida') {
-					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => red);
+					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => 'red');
 				}
 				else{
-					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => green);
+					$cell = array('data' => $acesso['sentido'], 'class' => 'sentido', 'background-color' => 'green');
 				}
 				$this->table->add_row($acesso['num_aluno'],$acesso['data'],$acesso['hora'],$acesso['porta'],$acesso['sentido'], 'Não');
 				
 			
 			}
 				
+			
 		}
-		
   		$this->table->set_template($template);
 
 		$data['table'] = $this->table->generate();
