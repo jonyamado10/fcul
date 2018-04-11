4b6e5cba9623cb5910c0d93ac86a94eb2f2ed44b
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-11 01:08:34 +0100

    gerar acessos

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -241,7 +241,7 @@ function array_flatten($array) {
   $result = array(); 
   foreach ($array as $key => $value) { 
     if (is_array($value)) { 
-      $result = array_merge($result, array_flatten($value)); 
+      $result = array_merge($result, $this->array_flatten($value)); 
     } 
     else { 
       $result[$key] = $value; 
