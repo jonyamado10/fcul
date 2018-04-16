551e6ea7d9ff788b9560f83176eab980c6ffda8f
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-16 21:59:17 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_alunos2.php
+++ application/views/tabela_alunos2.php
@@ -1,7 +1,14 @@
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.13/datatables.min.js"></script> 
+  <!-- Bootstrap core CSS-->
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/main.css") ?>">
+      <link rel="stylesheet" href="<?php echo base_url("assets/css/util.css") ?>">
+  <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="<?php echo base_url('assets/vendor/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="<?php echo base_url('assets/css/sb-admin.css') ?>" rel="stylesheet">
+  <!-- Page level plugin CSS-->
   <link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
-
+  
 <div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
