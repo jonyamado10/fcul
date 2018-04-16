b24c31b5209165e5a2e352590856db85fe302be3
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-17 00:59:51 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_acessos_alunos.php
+++ application/views/tabela_acessos_alunos.php
@@ -4,7 +4,6 @@
     <!-- Core plugin JavaScript-->
     <script src=<?php echo base_url("assets/vendor/jquery-easing/jquery.easing.min.js") ?>></script>
     <!-- Page level plugin JavaScript-->
-    <script src=<?php echo base_url("assets/vendor/chart.js/Chart.min.js") ?>></script>
     <script src=<?php echo base_url("assets/vendor/datatables/jquery.dataTables.js") ?>></script>
     <script src=<?php echo base_url("assets/vendor/datatables/dataTables.bootstrap4.js") ?>></script>
     <!-- Custom scripts for all pages-->
@@ -15,8 +14,6 @@
   <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
   <!-- Custom fonts for this template-->
   <link href="<?php echo base_url('assets/vendor/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css">
-  <!-- Custom styles for this template-->
-  <link href="<?php echo base_url('assets/css/sb-admin.css') ?>" rel="stylesheet">
   <!-- Page level plugin CSS-->
   <link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
 
