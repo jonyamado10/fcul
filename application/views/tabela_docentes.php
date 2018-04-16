945b0e44857db5be9f9513ee5ed111467a4ff2fa
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-16 23:27:20 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_docentes.php
+++ application/views/tabela_docentes.php
@@ -1,29 +1,5 @@
-    <!-- Bootstrap core JavaScript-->
-    <script src=<?php echo base_url("assets/vendor/jquery/jquery.min.js") ?>></script>
-    <script src=<?php echo base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js") ?>></script>
-    <!-- Core plugin JavaScript-->
-    <script src=<?php echo base_url("assets/vendor/jquery-easing/jquery.easing.min.js") ?>></script>
-    <!-- Page level plugin JavaScript-->
-    <script src=<?php echo base_url("assets/vendor/chart.js/Chart.min.js") ?>></script>
-    <script src=<?php echo base_url("assets/vendor/datatables/jquery.dataTables.js") ?>></script>
-    <script src=<?php echo base_url("assets/vendor/datatables/dataTables.bootstrap4.js") ?>></script>
-    <!-- Custom scripts for all pages-->
-    <script src=<?php echo base_url("assets/js/sb-admin.min.js") ?>></script>
-    <!-- Custom scripts for this page-->
-    <script src=<?php echo base_url("assets/js/sb-admin-datatables.min.js") ?>></script>
 
-     <!-- Bootstrap core CSS-->
-    <link rel="stylesheet" href="<?php echo base_url("assets/css/main.css") ?>">
-      <link rel="stylesheet" href="<?php echo base_url("assets/css/util.css") ?>">
-  <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
-  <!-- Custom fonts for this template-->
-  <link href="<?php echo base_url('assets/vendor/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css">
-  <!-- Custom styles for this template-->
-  <link href="<?php echo base_url('assets/css/sb-admin.css') ?>" rel="stylesheet">
-  <!-- Page level plugin CSS-->
-  <link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
-
-<div class="container-fluid">
+    <div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
@@ -34,27 +10,17 @@
       <!-- Example DataTables Card-->
       <div class="card mb-3">
         <div class="card-header">
-          <i class="fa fa-table"></i> Alunos</div>
+          <i class="fa fa-table"></i> Tabela Docentes</div>
         <div class="card-body">
           <div class="table-responsive">
-     
-    <table class="table table-bordered" id="tabela-docentes" width="100%" cellspacing="0">
-     <thead>
-     <tr><th>Nº Aluno</th>
-      <th>Nome</th>
-      <th>Email</th>
-      <th>Nº Cartão de Cidadão</th>
-      <th>Departamento</th></tr>
-     </thead>
-     <tbody>
-     </tbody>
-     </table>
+            <?php
+              echo $table;
+            ?>
 
               
           </div>
         </div>
         <div class="card-footer small text-muted"><?php date_default_timezone_set("Europe/Lisbon"); echo "Atualizado pela última vez às: " . date("G:i");?></div>
       </div>
-   </div>
-
+    </div>
 
