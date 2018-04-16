152fa012c61267aac4bddcad5bdb4d88dd22f26c
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-16 21:36:28 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_alunos2.php
+++ application/views/tabela_alunos2.php
@@ -7,12 +7,12 @@
         <li class="breadcrumb-item">
           <a href="#">Dashboard</a>
         </li>
-        <li class="breadcrumb-item active">Acessos</li>
+        <li class="breadcrumb-item active">Tabelas</li>
       </ol>
       <!-- Example DataTables Card-->
       <div class="card mb-3">
         <div class="card-header">
-          <i class="fa fa-table"></i> Tabela <?php echo $grupo;?></div>
+          <i class="fa fa-table"></i> Alunos</div>
         <div class="card-body">
           <div class="table-responsive">
      
@@ -34,5 +34,14 @@
         <div class="card-footer small text-muted"><?php date_default_timezone_set("Europe/Lisbon"); echo "Atualizado pela última vez às: " . date("G:i");?></div>
       </div>
    </div>
-
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#alunos-table').DataTable({
+        "ajax": {
+            url : "<?php echo site_url("books/books_page") ?>",
+            type : 'GET'
+        },
+    });
+});
+</script>
 
