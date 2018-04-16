025dd850847edb8348c7c127081e80a74f73a87f
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-16 22:37:39 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_alunos2.php
+++ application/views/tabela_alunos2.php
@@ -61,6 +61,8 @@
 $(document).ready(function() {
     $('#dataTable').DataTable({
         "ajax": {
+           paging: false,
+    searching: false,
             url : "<?php echo site_url("Tabelas/alunos") ?>",
             type : 'GET'
         },
