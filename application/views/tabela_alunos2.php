1f3b027eee77aeda072c680c5b5b49bc17bb618d
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-16 22:01:02 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_alunos2.php
+++ application/views/tabela_alunos2.php
@@ -1,4 +1,4 @@
-  <!-- Bootstrap core CSS-->
+              <!-- Bootstrap core CSS-->
     <link rel="stylesheet" href="<?php echo base_url("assets/css/main.css") ?>">
       <link rel="stylesheet" href="<?php echo base_url("assets/css/util.css") ?>">
   <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
@@ -8,7 +8,25 @@
   <link href="<?php echo base_url('assets/css/sb-admin.css') ?>" rel="stylesheet">
   <!-- Page level plugin CSS-->
   <link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
-  
+            <!-- Bootstrap core JavaScript-->
+    <script src=<?php echo base_url("assets/vendor/jquery/jquery.min.js") ?>></script>
+    <script src=<?php echo base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js") ?>></script>
+    <!-- Core plugin JavaScript-->
+    <script src=<?php echo base_url("assets/vendor/jquery-easing/jquery.easing.min.js") ?>></script>
+    <!-- Page level plugin JavaScript-->
+    <script src=<?php echo base_url("assets/vendor/chart.js/Chart.min.js") ?>></script>
+    <script src=<?php echo base_url("assets/vendor/datatables/jquery.dataTables.js") ?>></script>
+    <script src=<?php echo base_url("assets/vendor/datatables/dataTables.bootstrap4.js") ?>></script>
+    <!-- Custom scripts for all pages-->
+    <script src=<?php echo base_url("assets/js/sb-admin.min.js") ?>></script>
+    <!-- Custom scripts for this page-->
+    <script src=<?php echo base_url("assets/js/sb-admin-datatables.min.js") ?>></script>
+    <script src=<?php echo base_url("assets/js/sb-admin-charts.min.js") ?>></script>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.13/datatables.min.js"></script> 
+  <link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
+
 <div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
