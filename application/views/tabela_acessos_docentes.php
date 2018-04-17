8eca5a334ebd8f5ed33df20e4f176fd8546ad837
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-17 03:24:10 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_acessos_docentes.php
+++ application/views/tabela_acessos_docentes.php
@@ -16,7 +16,7 @@
         <div class="card-body">
           <div class="table-responsive">
      
-    <table class="table table-bordered" id="tabela-acessos-alunos" width="100%" cellspacing="0">
+    <table class="table table-bordered" id="tabela-acessos-docentes" width="100%" cellspacing="0">
      <thead>
    <tr>
 <th>Funcionário Nº</th><th>Nome</th><th>Data</th><th>Hora</th><th>Porta</th><th>Sentido</th><th>Passou Cartão?</th></tr>
