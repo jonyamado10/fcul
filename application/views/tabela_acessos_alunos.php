7f8417dc2b91a6a4ead020f71953b2ce3ca2c266
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-17 01:26:18 +0100

    tabela alunos- aumento de perfromance

--- application/views/tabela_acessos_alunos.php
+++ application/views/tabela_acessos_alunos.php
@@ -39,7 +39,10 @@
 
           <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
               <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
-
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
 <script type="text/javascript">
 $(document).ready(function() {
     $('#tabela-acessos-alunos').DataTable({
