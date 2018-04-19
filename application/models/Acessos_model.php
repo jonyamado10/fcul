5f12953db5e2c06b04d07c57a243fda16cd35eef
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-19 19:31:30 +0100

    algoritmo

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -223,7 +223,8 @@ class Acessos_model extends CI_Model {
 			 		
 			 	}
 			 	else{
-			 		if($acessos_por_pessoa[$i+1]['sentido'] == "Saida"){ // temos que simular uma entrada
+			 		if($acessos_por_pessoa[$i+1]['sentido'] == "Saida"){
+			 		echo "entrei"; // temos que simular uma entrada
 			 				$copia_acesso=$acessos_por_pessoa[$i+1];
 			 				$copia_acesso['sentido'] = "Entrada";
 			 				$copia_acesso['id_acesso'] = -$copia_acesso['id_acesso'];
@@ -236,6 +237,7 @@ class Acessos_model extends CI_Model {
 
 			 		}
 			 		else if($acessos_por_pessoa[$i+1]['sentido'] == "Entrada"){
+			 			echo "entrei";
 			 			if($acessos_por_pessoa[$i]['porta'] != $acessos_por_pessoa[$i+1]['porta'] ){
 			 				$copia_acesso= $acessos_por_pessoa[$i+1];
 			 				$copia_acesso['sentido'] = "Saida";
