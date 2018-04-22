ded99333571b54d64f31887300753198325754c0
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-22 21:17:21 +0100

    algoritmo

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -297,7 +297,7 @@ class Acessos_model extends CI_Model {
 				return $result;
 		}
 		function sum_time($horas, $op){
-			list($h, $m) = explode(':', '00:30');
+			list($h, $m) = explode(':', '00:10');
 			if($op = '+'){
 				return date('H:i', strtotime($horas) + $h*60*60 + $m*60); 
 			}
