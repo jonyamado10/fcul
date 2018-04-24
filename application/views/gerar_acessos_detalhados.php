9b7f4d880067db6df0c61a517b5f0c8951af9775
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-24 19:50:06 +0100

    acessos detalhados

--- application/views/gerar_acessos_detalhados.php
+++ application/views/gerar_acessos_detalhados.php
@@ -51,7 +51,7 @@
         <div class="wrap-input100 validate-input clockpicker3" data-placement="right" data-align="top" data-autoclose="true">
                 <input id = "name" type="text" class="input100" value="13:14">
           <span class="focus-input100"></span>
-          <label class="label-input100" for="name">
+          <label class="label-input100" for="name">Hora Inicial
             <span ><i class="fas fa-clock"></i></span>
           </label>
         </div>
