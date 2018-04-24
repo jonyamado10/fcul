ba3619597a92e520b5ed586e998fa119d90b89aa
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-24 22:38:07 +0100

    acessos detalhados

--- application/views/tabela_acessos_docentes.php
+++ application/views/tabela_acessos_docentes.php
@@ -36,10 +36,10 @@
 
 <script type="text/javascript">
     $('#tabela-acessos-docentes').DataTable({
-            "deferRender":    true,
-            "scrollY":        200,
-            "scrollCollapse": true,
-            "scroller":       true,
+            deferRender:    true,
+            scrollY:        200,
+            scrollCollapse: true,
+            scroller:       true,
         "ajax": {
 
             url : "<?php echo base_url("Tabelas/acessos_docentes") ?>",
