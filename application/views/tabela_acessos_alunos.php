9bc02822e61d39ca65126aeb1934016a56dfed86
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-25 18:29:39 +0100

    acessos detalhados

--- application/views/tabela_acessos_alunos.php
+++ application/views/tabela_acessos_alunos.php
@@ -12,7 +12,7 @@
           <i class="fa fa-table"></i> Alunos</div>
         <div class="card-body">
           <div class="table-responsive">
-
+<?php echo sizeof($acessos); ?>
     <table class="table table-bordered" id="tabela-acessos-alunos" width="100%" cellspacing="0">
      <thead>
    <tr>
