5e8ebae3ca6ff8a6017c9cbecae769d1de928186
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-25 17:51:41 +0100

    acessos detalhados

--- application/views/tabela_acessos_alunos.php
+++ application/views/tabela_acessos_alunos.php
@@ -44,7 +44,7 @@
          "url": "<?php echo base_url("Tabelas/acessos_alunos") ?>",
          "dataType": "json",
          "type": "POST",
-         "data":jsonData,
+         "data":{jsonData},
                        },
 
         "createdRow": function( row, data, dataIndex){
