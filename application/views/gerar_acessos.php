d87773460a9e14a0c78c00138a76604d3b46b08b
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-04-26 13:37:34 +0100

    versao actualizada

--- application/views/gerar_acessos.php
+++ application/views/gerar_acessos.php
@@ -48,6 +48,7 @@
                type: "POST",
                url: url,
                data: dataString, // serializes the form's elements.
+                        "data":{  '<?php echo $this->security->get_csrf_token_name(); ?>' : '<?php echo $this->security->get_csrf_hash(); ?>' },
                success: function(data)
                {
                    alert(data); // show response from the php script.
